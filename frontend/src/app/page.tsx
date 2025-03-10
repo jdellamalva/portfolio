79aaf33be6be85a05627670fc130e6036619783b
jdellamalva <59029670+jdellamalva@users.noreply.github.com>
2025-03-09 18:49:38 -0700

    making a loading animation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,32 @@
 import type { Metadata } from "next";
 
-export const metadata: Metadata = {
+import styles from "./page.module.css";
+import React from "react";
+
+export const metadata = {
   title: "JDM | Home",
   description: "Home page for Joseph D. Della Malva's portfolio site",
 };
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  const numPairs = 6;
+  return (
+    <div
+      className={styles.container}
+      style={{ "--num-pairs": numPairs } as React.CSSProperties}
+    >
+      <div className={styles.shapePairs}>
+        {[...Array(numPairs)].map((_, index) => (
+          <div
+            key={index}
+            className={styles.shapePair}
+            style={{ "--index": index } as React.CSSProperties}
+          >
+            <div className={styles.shape}></div>
+            <div className={styles.shape}></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
